31877ca15c2cd1ad9a1d89c07fb94e7f9cc022cc
Jayson Jacobs <jaysonjacobs@Hackintosh.local>
2018-02-23 08:27:47 -0700

    handle possible errors with 3rd party apis

--- routes/api.js
+++ routes/api.js
@@ -6,14 +6,46 @@ router.get('/bittrex/:first/:second', function(req, res) {
   console.log('timestamp:', Date.now());
   var first = decodeURIComponent(req.params.first).toUpperCase();
   var second = decodeURIComponent(req.params.second).toUpperCase();
-  request('https://bittrex.com/api/v1.1/public/getorderbook?market=' + first + '-' + second + '&type=both')
+  var options = {
+    url: 'https://bittrex.com/api/v1.1/public/getorderbook?market=' + first + '-' + second + '&type=both'
+  }
+  request(options, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      return body;
+    } else {
+      console.log(error);
+      return {
+        message: "error with bittrex api",
+        result: {
+          buy: [],
+          sell: []
+        },
+        success: false
+      };
+    }
+  })
   .pipe(res);
 });
 
 router.get('/poloniex/:first/:second', function(req, res) {
   var first = decodeURIComponent(req.params.first).toUpperCase();
   var second = decodeURIComponent(req.params.second).toUpperCase();
-  request('https://poloniex.com/public?command=returnOrderBook&currencyPair=' + first + '_' + second + '&depth=100')
+  var options = {
+    url: 'https://poloniex.com/public?command=returnOrderBook&currencyPair=' + first + '_' + second + '&depth=100'
+  };
+  request(options, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      return body;
+    } else {
+      console.log(error);
+      return {
+        asks: [],
+        bids: [],
+        isFrozen: "maybe",
+        seq: 0
+      };
+    }
+  })
   .pipe(res);
 });
 
@@ -21,7 +53,19 @@ router.get('/gemini/:first/:second', function(req, res) {
   //pairs are limited, not using this right now.
   var first = decodeURIComponent(req.params.first);
   var second = decodeURIComponent(req.params.second);
-  request('https://api.gemini.com/v1/book/' + second + first)
+  var options = {
+    url: 'https://api.gemini.com/v1/book/' + second + first
+  };
+  request(options, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      return body;
+    } else {
+      console.log(error);
+      return {
+        // put something here before enabling
+      };
+    }
+  })
   .pipe(res);
 });
 
@@ -34,8 +78,16 @@ router.get('/gdax/:first/:second', function(req, res) {
     headers: { 'User-Agent': 'Test Agent/0.0.1' }
   };
   request(options, function(error, response, body) {
-    //handle errors
-    return body;
+    if (!error && response.statusCode === 200) {
+      return body;
+    } else {
+      console.log(error);
+      return {
+        asks: [],
+        bids: [],
+        sequence: 0
+      };
+    }
   })
   .pipe(res);
 });
@@ -47,8 +99,20 @@ router.get('/cryptopia/:first/:second', function(req, res) {
     url: 'https://cryptopia.co.nz/api/GetMarketOrders/' + second + '_' + first
   }
   request(options, function(error, response, body) {
-    //handle errors
-    return body;
+    if (!error && response.statusCode === 200) {
+      return body;
+    } else {
+      console.log(error);
+      return {
+        Data: {
+          Buy: [],
+          Sell: []
+        },
+        Error: "response error",
+        Message: "response from back end not cryptopia",
+        Success: false
+      };
+    }
   })
   .pipe(res);
 });
